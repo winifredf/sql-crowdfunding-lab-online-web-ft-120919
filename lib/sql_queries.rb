a15a9929f7087396e974a22af61c878569cca0e3
Winifred Frierson <winifred.dykes@hotmail.com>
2020-02-10 03:32:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  SELECT * FROM projects, pledges
+  SELECT projects, pledges
   
   "Write your SQL query Here"
 end
