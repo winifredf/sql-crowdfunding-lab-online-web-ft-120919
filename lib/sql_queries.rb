c347bd17a1207ba15372c88f282d583de4c1a173
Winifred Frierson <winifred.dykes@hotmail.com>
2020-02-10 03:42:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   SELECT projects.title, pledges.amount,
     FROM projects,
     INNER JOIN  pledges,
-    ON 
+    ON pledges.project_id = 
   
   "Write your SQL query Here"
 end
